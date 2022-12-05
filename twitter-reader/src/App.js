1a8304b2d0e95615d2c0e87a93967c82ea33fe1d
James Shapiro <jamesshapiro@gmail.com>
2022-12-05 07:22:24 -0500

    add second attempt with Flexbox

---
+++ twitter-reader/src/App.js
@@ -0,0 +1,24 @@
+import Tweet from './Tweet'
+import './tweet.css'
+
+import { createRoot } from 'react-dom/client'
+
+const App = () => {
+  return (
+    <div>
+      <Tweet
+        username="jamesshapiro"
+        userDisplayName="James Shapiro"
+        text="Trying out an example tweet. This tweet will be 280 characters long to test out the new and improved longer-tweet format. Mostly I'm just trying to see how it looks. 280 characters is a lot of characters and I plan to use all of them. 280 280 280 280 280 280 280 280!"
+        date="December 05, 2022"
+        likes="100,000"
+        retweets="10,000"
+        replies="1,000"
+      />
+    </div>
+  )
+}
+
+createRoot(document.getElementById('root')).render(<App />)
+
+export default App
---
+++ twitter-reader/src/Tweet.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Tweet = ({
+  username,
+  userDisplayName,
+  text,
+  date
+}) => {
+  const relativeDate = (date) => {
+    // Get the current date and time
+    const now = new Date()
+
+    // Calculate the difference between the given date and the current date
+    const diff = now - date
+
+    // Convert the difference to hours
+    const hours = Math.floor(diff / (60 * 60 * 1000))
+
+    // If the difference is less than 24 hours, return the difference in hours
+    if (hours < 24) {
+      return `${hours}h`
+    }
+
+    // Otherwise, return the full date
+    return date.toDateString()
+  }
+  return (
+    <div className="tweet" style={{ display: 'flex' }}>
+      <img
+        className="avatar"
+        src={`https://pbs.twimg.com/profile_images/1267830833940815872/gN21yPbi_400x400.jpg`}
+        alt={username}
+        style={{ flex: '0 0 auto' }}
+      />
+      <div className="tweet-content" style={{ flex: '1 1 auto' }}>
+        <div className="tweet-header">
+          <div className="tweet-username">
+            <span className="display-name">{userDisplayName}</span> @{username}{' '}
+            · <span className="tweet-date">{relativeDate(date)}</span>
+          </div>
+        </div>
+        <div className="tweet-text">{text}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Tweet
+
+
+
